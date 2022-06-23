184cf45c9526817b5fc5fec3f90eb70eb42945e9
OmFrederic <frederic.ballllosera@gmail.com>
2022-06-23 11:29:30 +0200

    Fixing formatter requirements

--- bioimageio.napari/__init__.py
+++ bioimageio.napari/__init__.py
@@ -1,5 +1,1 @@
 """Implement bioimageio.napari."""
-
-from .model_manager import get_models_path, set_models_path, get_rdf_url, set_rdf_url, \
-    get_model_list, get_installed_models, \
-    install_model, inspect_model, remove_model, load_model, convert_model_to_yaml_string
